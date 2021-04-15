c03a9fa02ba1de3720be25f513089d1ed54db929
Ayumi Watanabe <ayumiwatanabe@ayuminoMacBook-Pro.local>
2021-04-15 21:32:01 +0900

    Can you check it, please.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'home/top'
+  get 'static_pages/new'
+  get 'static_pages/index'
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root  'static_pages#home'
+
+get '/about', to: 'static_pages#about'
+get '/contact', to: 'static_pages#contact'
 end
+
+
