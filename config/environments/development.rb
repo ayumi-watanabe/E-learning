caa8464575e621ea006980c87fc21a1196ad2b9f
Ayumi Watanabe <ayumiwatanabe@ayuminoMacBook-Pro.local>
2021-04-27 21:23:39 +0900

    login page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -51,6 +50,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.reload_classes_only_on_change = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root  'static_pages#home'
+  root  'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
-get '/about', to: 'static_pages#about'
-get '/contact', to: 'static_pages#contact'
-
-#signup(localhost:3002)
-get '/signup', to: 'users#new'
-
-get '/signup', to: 'user#new'
-
+  resources :users
+  get '/signup', to: 'users#new'
 end
