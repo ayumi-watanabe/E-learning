70767ec688fc75305f47db5cf0a02d26a7c4da2c
ayumi-watanabe <79447745+ayumi-watanabe@users.noreply.github.com>
2021-04-28 20:52:26 +0900

    Sign up page (#6)
    * Sign up page
    
    * how do i connect to mysql
    
    * please check gemfile
    
    * still db isn't working
    
    * login page
    
    * adding back application_controller
    
    * fixed error
    
    Co-authored-by: Ayumi Watanabe <ayumiwatanabe@ayuminoMacBook-Pro.local>

--- Gemfile
+++ Gemfile
@@ -25,7 +25,16 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+ gem 'bcrypt', '~> 3.1.7'
+
+# Bootstrap - source: https://github.com/twbs/bootstrap-rubygem
+gem 'bootstrap', '~> 4.4.1'
+gem 'jquery-rails'
+gem 'faker', '~> 1.6', '>= 1.6.6'
+gem 'will_paginate', '~> 3.2', '>= 3.2.1'
+gem 'will_paginate-bootstrap4', '~> 0.2.2'
+
+#bundle install
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.paginate(page:params[:page], per_page: 10)
+  end
+
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success]="Saved successfully!"
+      redirect_to root_url
+    else
+      flash[:danger]="Invalid content. Try again"
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params:[:id])
   end
 
-  def create
-    @user = User.new(parmas[:user])
-    if @user.save
-    flash[:success]="Saved successfully!"
-    redirect_to login_url
-    else
-    flash[:danger]="Invalid content. Try again"
-      render 'new'  
+  private
+    def user_params 
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  end
 
 end
