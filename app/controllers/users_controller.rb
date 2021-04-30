281b7939e9753e7dc1630ea191dc84aa73b03dc0
ayumi-watanabe <peanuts.ayumi.9619@gmail.com>
2021-04-30 21:14:30 +0900

    fixed it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only:[:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.paginate(page:params[:page], per_page: 10)
   end
