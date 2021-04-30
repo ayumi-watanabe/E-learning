35829d29b6a651b04e30905631a0626fcfb05fd7
ayumi-watanabe <79447745+ayumi-watanabe@users.noreply.github.com>
2021-04-30 21:56:40 +0900

    settings_pages (#8)
    * settings_pages
    
    * fixed it
    
    * fixed it
    
    * fixed it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only:[:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.paginate(page:params[:page], per_page: 10)
   end
@@ -22,9 +24,34 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success]= "Save successfully!"
+       redirect_to @user
+    else
+      flash[:danger] = "Invalid content. Try again."
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to user_url
+  end
+
   private
     def user_params 
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end
