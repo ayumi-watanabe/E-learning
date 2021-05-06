e0392d0e123b48c975cdbf4a97770babf76232b3
ayumi-watanabe <peanuts.ayumi.9619@gmail.com>
2021-05-06 15:31:40 +0900

    categories_page

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,28 @@
+class Admin::CategoriesController < ApplicationController
+  #before_action :admin_log_in
+
+  def index
+    @categories = Category.all
+  end
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success]="Saved successfully!"
+      redirect_to root_url
+    else
+      flash[:danger]="Invalid content. Try again"
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
